7c8f7488f63688a6c5a6e5488222f6d3873546eb
reo <sodapon03@gmail.com>
2025-11-08 08:35:33 +0900

    Initial Commit

---
+++ Marubatsu/MarubatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarubatsuApp.swift
+//  Marubatsu
+//
+//  Created by 田口怜央 on 2025/11/08.
+//
+
+import SwiftUI
+
+@main
+struct MarubatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
